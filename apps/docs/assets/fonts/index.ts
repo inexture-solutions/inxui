786ad240ffd8975ab65da0b1ef2158f1d8e7ef38
m4st3rai <developer@inexture.com>
2024-10-29 17:50:26 +0530

    initial commit

--- apps/docs/assets/fonts/index.ts
+++
@@ -1,9 +0,0 @@
-import { Montserrat as BaseGoogleFont } from "next/font/google";
-
-export const baseFont = BaseGoogleFont({
-  subsets: ["latin"],
-  preload: true,
-  display: "swap",
-  variable: "--base-font",
-  weight: ["500", "600", "700"],
-});
