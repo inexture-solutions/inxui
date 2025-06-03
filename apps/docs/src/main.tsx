3e82844f8122905c2d2d1382dee201011735d3b5
m4st3rai <prafulla@inexture.com>
2025-06-03 14:35:25 +0530

    Packages and internal deps updated

--- apps/docs/src/main.tsx
+++ apps/docs/src/main.tsx
@@ -1,9 +1,14 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
+import { ThemeProvider } from "@inexture/base/providers";
+import { ThemeCustomizer } from "@inexture/base/theme";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <ThemeProvider>
+      <ThemeCustomizer />
+      <App />
+    </ThemeProvider>
   </StrictMode>,
 );
